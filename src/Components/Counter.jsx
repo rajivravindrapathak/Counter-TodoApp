09c8e58eeb78ff0a9d8ee2fd405796fd7f93373d
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-11 15:48:26 +0530

    Add files via upload

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
-const Counter = () => {
-  return (
-    <div>
-      counter
-    </div>
-  )
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            counter: 0
+        }
+    }
+
+
+    handleCounterChange = (value) => {
+        // this.setState({
+        //     counter: value + this.state.counter
+        // }, ()=>console.log(this.state))
+
+        // OR
+        this.setState((prev) => {
+            return {
+                counter: prev.counter + value
+            }
+        })
+    }
+
+    render() {
+        const { title } = this.props
+        const { counter } = this.state
+        return (
+            <div>
+                <h1>{ title }</h1>
+                <h1>{ counter }</h1>
+                <button onClick={() => this.handleCounterChange(1)}>Add</button>
+                <button onClick={() => this.handleCounterChange(-1)}>Reduce</button>
+            </div>
+        )
+    }
 }
+
 
 export default Counter
