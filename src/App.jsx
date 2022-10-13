cbe4f0f5d14f5576cd126fc8b86ccd5031894d25
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-13 07:52:26 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Counter />
-      {/* <Todo /> */}
+      <Todo />
     </div>
   )
 }
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
-const Todo = () => {
-  return (
-    <div>
-      todo
-    </div>
-  )
+export class Todo extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      todo: [],
+      query: ""
+    }
+  }
+
+  handleChange (e) {
+    console.log(this)
+    this.setState({
+      query: e.target.value
+    })
+  }
+
+  render () {
+    const { query, todo } = this.state
+
+    return (
+      <>
+        <div>
+          <h1> Todo App </h1>
+          <input value={query} onChange={this.handleChange} placeholder="add something" />
+          <button>Add Todo</button>
+        </div>
+      </>
+    )
+  }
 }
+
 
 export default Todo
