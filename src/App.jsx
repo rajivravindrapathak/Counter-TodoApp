28d72945457aae2b6ac0a277b8f3628969650d9e
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-17 11:47:33 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Counter from './Components/Counter'
-import Todo from './Components/Todo'
+import Counter from "./Components/Counter"
+import { Todo } from './Components/Todo'
 
 function App() {
 
   return (
     <div className="App">
+      <h1>Counter for my app</h1>
       <Counter />
+      <h1>Todo app</h1>
       <Todo />
     </div>
   )
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,44 +1,36 @@
 import React from 'react'
 
-class Counter extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            counter: 0,
-            totalClicks: 0
+    // sugar syntax
+    class Counter extends React.Component {
+        constructor(props) {
+            super(props)
+            //initialize
+            //state management
+            this.state = {
+                counter: 0
+            }
+            // single object
         }
-    }
-
-
-    handleCounterChange = (value) => {
-        this.setState({
-            counter: value + this.state.counter,
-            totalClicks: this.state.totalClicks + 1
-        }, ()=>console.log(this.state))
-
-        // OR
-        // this.setState((prev) => {
-        //     return {
-        //         counter: prev.counter + value
-        //     }
-        // })
-    }
+        handleCounterChange = (value) => {
+            //patch
+            this.setState({
+                counter: value + this.state.counter
+            })
+        }
 
     render() {
         const { title } = this.props
-        const { counter, totalClicks } = this.state
+        const { counter } = this.state
+    
         return (
             <div>
-                <h1>{ title }</h1>
-                <h1>{ counter }</h1>
+                <h1>{ title } </h1>
+                <h2> {counter} </h2>
                 <button onClick={() => this.handleCounterChange(1)}>Add</button>
                 <button onClick={() => this.handleCounterChange(-1)}>Reduce</button>
-                <div> Total clicks = {this.state.totalClicks} </div>
             </div>
         )
     }
 }
-
 
 export default Counter
